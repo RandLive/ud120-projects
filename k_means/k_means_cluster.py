f2b428b6c355997ac1fe6b040d19a5dcec686a11
RandLive <dreamflg_dv@hotmail.com>
2017-04-17 21:13:29 +0200

    0417

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -8,16 +8,18 @@
 
 
 import pickle
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
+from sklearn.preprocessing import MinMaxScaler
 
 
 
-def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2", f3_name="feature 3"):
     """ some plotting code designed to help you visualize your clusters """
 
     ### plot each cluster with a different color--add more colors for
@@ -45,27 +47,57 @@ data_dict.pop("TOTAL", 0)
 
 
 ### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
+from sklearn.preprocessing import MinMaxScaler
+stock = []
+for i in data_dict:
+    if (data_dict[i]["exercised_stock_options"]=='NaN'):
+        #stock.append(0.0)
+        pass
+    else:
+        stock.append(float(data_dict[i]["exercised_stock_options"]))
+ma = max(stock)
+mi = min(stock)
+print "Exercised stock options maximum: ", ma, " minimum: ", mi
+# maximum:  34348384.0  minimum:  3285.0 comment out line 108 to get rid of zeroes.
+# print float(1000000-mi)/(ma-mi)
+
+salary = []
+for i in data_dict:
+    if (data_dict[i]["salary"]=='NaN'):
+        #stock.append(0.0)
+        pass
+    else:
+        salary.append(float(data_dict[i]["salary"]))
+ma = max(salary)
+mi = min(salary)
+print "Exercised salary options maximum: ", ma, " minimum: ", mi
+# print float(1000000-mi)/(ma-mi)
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
 for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+    plt.scatter( f1, f2)
 plt.show()
+
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+from sklearn.cluster import KMeans
+clf = KMeans(n_clusters=2,
+             max_iter=500,
+             init="k-means++")
+pred = clf.fit_predict( finance_features )
 
 
 ### rename the "name" parameter when you change the number of features
